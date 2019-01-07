34b948e4ed44e4fa2222856274c564c3e605f5ca
DLitka <dcywinskalitka@sigma.ug.edu.pl>
2019-01-08 00:11:53 +0100

    change calculator selector to switch, remove unnecesary code

--- valid.rb
+++ valid.rb
@@ -1,21 +1,20 @@
 require 'set'
 load 'slowo_do_daty.rb'
 load 'data_do_slowa.rb'
-#require 'slowo_do_daty'
+
 puts "WITAJ W ANALIZATORZE DAT! "
 puts "Z której opcji chcesz skorzystać?"
 puts " 1 - konwersja daty na słowa" 
 puts " 2 - konwersja słowa na datę" 
 
-abc = gets 
-abc = abc.chomp
-if abc.to_s == "1"
+case gets.chomp
+when "1"
 	puts "Podaj swoją datę urodzenia"  	#communicate
 	input = gets 				#getting date of birth from user
 	c = DataDoSlowa.new(input)
 	c.sprawdzDate
 
-elsif abc.to_s == "2"
+when "2"
 	puts "Podaj słowo do konwersji: "
 	input = gets
 	calculator= SlowoDoDaty.new(input)
@@ -26,7 +25,3 @@ else
   puts "Błędny wybór"
 
 end 
-
-
-
-
